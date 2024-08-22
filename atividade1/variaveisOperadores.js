202b4153e595aac43f9d458da59c847ff9de8e6f
BrenoCamaro <breno.camaro.5@gmail.com>
2024-08-22 15:15:25 -0300

    Atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,33 @@
+// Atividade 01
+// Autor: Breni Camarô Arraes
+// Faça a declaração de 2 variáveis numéricas para cada operador e guarde o resultado da operação em uma terceira variável
+// Para cada resultado obtido, imprima o texto “Resultado da {operação} entre {num_A} e {num_B} é {resultado}”
+
+//Soma
+let num1 = 1;
+let num2 = 2;
+let operacao1 = "soma";
+let result1 = num1 + num2;
+
+//Subtração
+let num3 = 4;
+let num4 = 4;
+let operacao2 = "subtração";
+let result2 = num1 - num2;
+
+//Multiplicação
+let num5 = 5;
+let num6 = 6;
+let operacao3 = "multiplicação";
+let result3 = num1 * num2;
+
+//Divisão
+let num7 = 7;
+let num8 = 8;
+let operacao4 = "divisão";
+let result4 = num1 / num2;
+
+console.log(`Resultado da ${operacao1} entre ${num1} e ${num2} é ${result1}.`);
+console.log(`Resultado da ${operacao2} entre ${num3} e ${num4} é ${result2}.`);
+console.log(`Resultado da ${operacao3} entre ${num5} e ${num6} é ${result3}.`);
+console.log(`Resultado da ${operacao4} entre ${num7} e ${num8} é ${result4}.`);
